8542c54c4eb209914fdef4a38551fd6f83c75800
Cheezily <pm655@yahoo.com>
2016-02-13 12:24:44 -0600

    remove users reference from app

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/idcheck', idCheck);
 app.use('/dbpull', dbpull);
 
