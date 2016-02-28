bb1fb2ab65130606e6db48164024880aba2925b5
= <=>
2016-02-28 16:47:43 -0600

    stopped export of db id

--- routes/dbpull.js
+++ routes/dbpull.js
@@ -13,7 +13,7 @@ if (process.env.DBPULL_PW) {
   password = 'password';
 }
 
-var fields = ['id', 'userID', 'traits1', 'traits2', 'traits3', 'issues', 'clarity',
+var fields = ['userID', 'traits1', 'traits2', 'traits3', 'issues', 'clarity',
   'prefer', 'enthusiasm', 'peers', 'peer_enthusiasm', 'hillary_clinton_ideology',
   'bernie_sanders_ideology', 'ted_cruz_ideology',
   'donald_trump_ideology', 'john_kasich_ideology', 'ben_carson_ideology',
